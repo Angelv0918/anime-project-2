f405d81ef0bdf20d2c67670f75206a4ab2feb875
angelv18 <angelvasquez0918@gmail.com>
2022-11-22 13:40:09 -0500

    basic express app

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+let PORT = 3000;
+if(process.env.PORT){
+	PORT = process.env.PORT
+}
+
+app.get('/', (req, res)=>{
+	res.send('hi');
+})
+
+app.listen(PORT, () => {
+  console.log('listening...');
+})
